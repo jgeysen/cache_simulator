3d451855abde720123983a1e49708006f6648acd
Jori Geysen <jori.geysen@eigentech.com>
2023-03-25 14:59:22 +0000

    add cli arguments with getopt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 // LRU Cache simulator
 use std::fs::File;
 use std::io::{ self, BufRead, BufReader };
+use getopt::Opt;
 
-fn type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
+fn _type_of<T>(_: &T) -> String {
+    return std::any::type_name::<T>().to_string()
 }
 
 fn read_lines(filename: String) -> io::Lines<BufReader<File>> {
@@ -13,12 +14,46 @@ fn read_lines(filename: String) -> io::Lines<BufReader<File>> {
     return io::BufReader::new(file).lines();
 }
 
-fn main() {
-    // Stores the iterator of lines of the file in lines variable.
-    let lines = read_lines("./traces/trans.trace".to_string());
-    // Iterate over the lines of the file, and in this case print them.
-    for line in lines {
-        type_of(&line);
-        println!("{}", line.unwrap());
+fn get_cli_arguments() -> Result<(i32, i32, i32), getopt::Error>{
+    let args: Vec<String> = std::env::args().collect();
+    let mut opts = getopt::Parser::new(&args, "s:b:e:");
+
+    let mut s = String::new();
+    let mut b = String::new();
+    let mut e = String::new();
+
+    loop {
+        match opts.next().transpose()? {
+            None => break,
+            Some(opt) => match opt {
+                Opt('s', Some(arg)) => s = arg.clone(),
+                Opt('b', Some(arg)) => b = arg.clone(),
+                Opt('e', Some(arg)) => e = arg.clone(),
+                _ => unreachable!(),
+            },
+        }
     }
+    let s = s.parse::<i32>().unwrap();
+    let b = b.parse::<i32>().unwrap();
+    let e = e.parse::<i32>().unwrap();
+
+//     println!("Option s: {}, {}", s, type_of(&s));
+//     println!("Option b: {}, {}", b, type_of(&b));
+//     println!("Option e: {}, {}", e, type_of(&e));
+
+    return Ok((s, b, e))
+}
+
+fn main() -> Result<(), getopt::Error>{
+    // Stores the iterator of lines of the file in lines variable.
+    let _reader = read_lines("./traces/trans.trace".to_string());
+    let (_s, _b, _e) = get_cli_arguments().unwrap();
+
+    // Iterate over the lines of the file, and in this case print them.
+//     for line in lines {
+//         type_of(&line);
+//         println!("{}", line.unwrap());
+//     }
+
+    Ok(())
 }
