3a1603141927123326e6f39f86ba04380ceccd2d
Jori Geysen <jori.geysen@eigentech.com>
2023-03-25 00:59:29 +0000

    add trace file reading to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// LRU Cache simulator
+use std::fs::File;
+use std::io::{ self, BufRead, BufReader };
+
+fn type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+fn read_lines(filename: String) -> io::Lines<BufReader<File>> {
+    // Open the file in read-only mode.
+    let file = File::open(filename).unwrap();
+    // Read the file line by line, and return an iterator of the lines of the file.
+    return io::BufReader::new(file).lines();
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Stores the iterator of lines of the file in lines variable.
+    let lines = read_lines("./traces/trans.trace".to_string());
+    // Iterate over the lines of the file, and in this case print them.
+    for line in lines {
+        type_of(&line);
+        println!("{}", line.unwrap());
+    }
 }
