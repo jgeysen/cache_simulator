6a2badf26d512ea0d13cf5a724de4ce9fe96f68a
Jori Geysen <jori.geysen@eigentech.com>
2023-04-04 01:10:50 +0100

    finish preprocessing of address

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,41 @@ use std::io::{ self, BufRead, BufReader };
 use std::env;
 use getopt::Opt;
 use regex::Regex;
-use ascii_converter::hexadecimal_to_binary;
 
 fn _type_of<T>(_: &T) -> String {
     return std::any::type_name::<T>().to_string()
+}
+
+fn convert_to_binary_from_hex(hex: &str) -> String {
+    hex.chars().map(to_binary).collect()
+}
+
+fn to_binary(c: char) -> &'static str {
+    match c {
+        '0' => "0000",
+        '1' => "0001",
+        '2' => "0010",
+        '3' => "0011",
+        '4' => "0100",
+        '5' => "0101",
+        '6' => "0110",
+        '7' => "0111",
+        '8' => "1000",
+        '9' => "1001",
+        'A' => "1010",
+        'B' => "1011",
+        'C' => "1100",
+        'D' => "1101",
+        'E' => "1110",
+        'F' => "1111",
+        'a' => "1010",
+        'b' => "1011",
+        'c' => "1100",
+        'd' => "1101",
+        'e' => "1110",
+        'f' => "1111",
+        _ => "",
+    }
 }
 
 fn read_lines(filename: String) -> io::Lines<BufReader<File>> {
@@ -43,8 +74,12 @@ fn get_cli_arguments() -> Result<(i32, i32, i32), getopt::Error>{
     return Ok((s, b, e))
 }
 
-fn preprocess_line(line: &String, _s: &i32, _b: &i32) {
+fn preprocess_line(line: &String, s: &i32, b: &i32) -> (String, String, String) {
     let general_re = Regex::new(r"^ [LMS] [0-9a-fA-F]+,[0-9]+$").unwrap();
+
+    let code = "";
+    let set_index = "";
+    let tag = "";
 
     if line.starts_with("I") {
     } else if general_re.is_match(&line) {
@@ -52,35 +87,43 @@ fn preprocess_line(line: &String, _s: &i32, _b: &i32) {
         let re = Regex::new(r"^ ([LMS]) ([0-9a-fA-F]+),[0-9]+$").unwrap();
         let code = re.captures(&line).unwrap().get(1).map_or("", |m| m.as_str());
         let address = re.captures(&line).unwrap().get(2).map_or("", |m| m.as_str());
-        let address_hex_vec: &Vec<_> = &address.matches(char::is_alphanumeric).collect();
-        let doubled: Vec<String> = address_hex_vec.into_iter().map(|x| x.to_string()).collect();
-
-        let address_bin = hexadecimal_to_binary(&doubled).unwrap();
-
-        println!("Code: {}\nAddress: {:?}", &code, &address);
-        println!("Address in binary: {:?}", &address_bin);
 
         // Translate memory address to binary:
+        let address_binary = convert_to_binary_from_hex(&address);
+        println!("Code: {}\nAddress: {:?}\nAddress in binary: {:?}", &code, &address, &address_binary);
 
-        // from memory address, find the tag
+        // from binary memory address, find the tag
+        let addres_size = &address_binary.len();
+        let addres_size = *addres_size as i32;
+        let tag_length = &addres_size - b - s;
+        let tag_length = tag_length as usize;
+        println!("tag_length: {}", &tag_length);
+        let tag = &address_binary[0..tag_length];
+        println!("tag: {}", &tag);
 
-
-        // from memory address, find the set_index
-
+        // from binary memory address, find the set_index
+        let set_length = &addres_size - b;
+        let set_length = set_length as usize;
+        println!("set_length: {}", &set_length);
+        let set_index = &address_binary[tag_length..set_length];
+        println!("set_index: {}", &set_index);
+        return (code.to_string(), tag.to_string(), set_index.to_string())
 
     }
-//     return (code, tag, set_index)
+    return (code.to_string(), tag.to_string(), set_index.to_string())
+
 }
 
 fn main() -> Result<(), getopt::Error>{
     env::set_var("RUST_BACKTRACE", "1");
     // Stores the iterator of lines of the file in lines variable.
-    let reader = read_lines("./traces/trans.trace".to_string());
+    let reader = read_lines("./traces/yi2.trace".to_string());
     let (s, b, _e) = get_cli_arguments().unwrap();
 
     // Iterate over the lines of the file, and in this case print them.
     for line in reader {
-        preprocess_line(&line.unwrap(), &s, &b)
+        let (code, tag, set_index) = preprocess_line(&line.unwrap(), &s, &b);
+
 //         process_line(code, tag, set_index)
     }
 
